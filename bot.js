78d85bb5ce6b582d02682774cbf6188a1db97acb
Usuario <usuario@MacBook-Pro.local>
2020-01-16 23:25:26 -0300

    wip

--- bot.js
+++ bot.js
@@ -1,32 +1,60 @@
-var Twit = require('twit')
+const Twit = require('twit')
+const express = require('express')
+const app = express()
 
 
 
-var T = new Twit({
+const bot = new Twit({
     consumer_key: 'dQiM3LFfBH9WL0fFqZ9UW4LRA',
     consumer_secret: 'TspJLkheHlm89vt3dLkVXaitMVFj2iSYNBfm07mQIQfmwubMz4',
     access_token: '1119352323178926081-yVuRyRZDFFQNEeuYFKaLrNUxY9eSEX',
     access_token_secret: 'QVHkfbpnd3wAGcGy1ZMnk19NU3emTgBV3bLDPTh3nFRh6',
     timeout_ms: 60 * 1000,  // optional HTTP request timeout to apply to all requests.
-    strictSSL: true,     // optional - requires SSL certificates to be valid.
-})
-
-//
-//  tweet 'hello world!'
-//
-
-/*T.post('statuses/update', { status: 'hello world!' }, function (err, data, response) {
-    console.log(data)
-})*/
-
-T.get('search/tweets', { q: 'xptr123 since:2018-07-11', count: 100 }, function (err, data, response) {
-    console.log(data)
-    //console.log(response)
+    strictSSL: true,     // optional - requires SSL certificates to be valid..
 })
 
 
+let jsonData
 
-T.post('statuses/update', {
+const tweetsList = []
+
+function answerTweet(tweet) {
+    const answer = {
+        status: 'Olá @' + 'DoggoTheBot',
+        in_reply_to_status_id: '' + '1217989043377582080'
+    }
+
+    bot.post('statuses/update', answer, (err, data, response) => console.log(err))
+}
+
+
+/*async function searchTweet() {
+     bot.get('search/tweets', { q: 'oieusouogoku since:2011-07-11', count: 1 }, function (err, data, response) {
+        const { statuses } = data
+
+        jsonData = statuses.map(tweet => tweetsList.push(tweet))
+        jsonData = statuses;
+    })
+}*/
+
+
+
+//answerTweet('a')
+
+app.get('/data', (req, res) => {
+    return res.json(tweetsList)
+})
+
+app.listen(3333);
+
+
+
+
+
+
+
+
+/*bot.post('statuses/update', {
     status: '@Everelindo Teste!',
     in_reply_to_status_id: '1151663004326539265'
 }, function (err, data, response) {
@@ -35,7 +63,7 @@ T.post('statuses/update', {
     } else {
         console.log(data.text + ' tweeted!')
     }
-})
+})*/
 
 
 
